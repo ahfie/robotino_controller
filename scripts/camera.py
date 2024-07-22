10ef716d9f84c7bebc73755b7029db7101d71ce2
ahfie <ahfieywf@gmail.com>
2024-07-22 14:11:07 +0200

    add camera publisher

---
+++ scripts/camera.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import cv2
+import argparse
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+cap  = ''
+stream_url = "http://131.159.213.47:8080/video"
+
+rospy.init_node('camera_publisher', anonymous=True)
+image_pub = rospy.Publisher('camera/image', Image, queue_size=1)  # Adjust topic name if necessary
+bridge = CvBridge()
+
+def spin():
+    rate = rospy.Rate(15)
+
+    while not rospy.is_shutdown():
+        ret, frame = cap.read()
+        if not ret:
+            rospy.INFO("Error: Could not read frame")
+            break
+
+        try:
+            ros_image = bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+        except Exception as e:
+            rospy.logerr(f"Error converting image: {e}")
+            continue
+
+        image_pub.publish(ros_image)
+        rate.sleep()
+    
+    cap.release()
+    rospy.INFO("ros exited from camera")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Command-line argument parsing to get ip and port of camera streaming.")
+
+    parser.add_argument('--ip', type=str, default="131.159.213.47")
+    parser.add_argument('--port', type=str, default="8080")
+
+    args = parser.parse_args()
+
+    # URL of the video stream
+    stream_url = 'http://' + args.ip + ':' + args.port + '/video'
+    cap = cv2.VideoCapture(stream_url)
+    # Check if the stream is opened
+    if not cap.isOpened():
+        rospy.INFO("Error: Could not open video stream")
+        exit()
+
+    try:
+        spin()
+    except rospy.ROSInterruptException:
+        cap.release()
+        rospy.INFO("error: rospy.ROSInterruptException from camera.py")
--- scripts/pid.py
+++ scripts/pid.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
 class PID:
-    def __init__(self, kp, ki, kd):
+    def __init__(self, kp, ki, kd, set_point=0.0):
         self.kp = kp
         self.ki = ki
         self.kd = kd
+        self.prev_error = 0.0
+        self.integral = 0.0
+        self.set_point = set_point
+
+    def set_point(self, set_point):
+        self.set_point = set_point
+        self.integral = 0.0
+        self.prev_error = 0.0
+
+        
